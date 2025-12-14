a22f231fb7bf311b4b87772c34619b52e75b2379
deep314313 <d.gupta9868@gmail.com>
2025-12-14 11:04:17 +0530

    one line

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../context/AuthContext'
 import { CartProvider } from '../context/CartContext'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
